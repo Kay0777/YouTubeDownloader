26a0afd1a1a821bfa7e665f2de61b90e140b42c5
Kayrat Urazov <>
2020-09-16 16:34:51 +0500

    Main working place

---
+++ main.py
@@ -0,0 +1,74 @@
+from PyQt5.QtWidgets import QApplication
+from core import MyCoreWindow, Loader
+from pytube import YouTube
+from threading import Thread
+import sys
+import re
+import os
+import time
+
+REGEXs = [
+    "http[s]?://(?:[a-zA-Z]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
+    "^www?.(?:[-\w.]|[$-_@.&+]|[!*\(\),])*"
+]
+
+URL = ''
+RES = ''
+OUTPUT_URL = ''
+
+
+class YouTubeK(MyCoreWindow):
+    def __init__(self):
+        super().__init__()
+        self.url.textChanged.connect(self.clear)
+        self.download.clicked.connect(self.download_video)
+
+        # https://youtu.be/fShTCJrNgxA
+
+    def clear(self):
+        if self.url.text() == '':
+            self.status.setText('')
+
+    def download_video(self):
+        self.loader = Loader()
+        self.get_output_path()
+
+        def download_now():
+            global URL, RES, OUTPUT_URL, REGEXs
+
+            URL = self.url.text()
+            RES = self.format.currentText()
+            self.status.setText("Please! Wait")
+            if (re.search(REGEXs[0], URL) or re.search(REGEXs[1], URL)):
+                try:
+                    yt = YouTube(url=URL)
+                    video = yt.streams.filter(
+                        file_extension='mp4',
+                        res=f'{RES}',
+                        progressive="True")[0]
+                    video.download(output_path=OUTPUT_URL)
+                    self.status.setText("Video downloaded")
+                except Exception as e:
+                    print(f'{e}')
+                    self.status.setText('Not found!')
+            else:
+                self.status.setText('Please! Check url')
+            time.sleep(0.5)
+            self.loader.close()
+
+        download = Thread(target=download_now)
+        download.start()
+
+    def get_output_path(self):
+        global OUTPUT_URL
+        OUTPUT_URL = 'C:/Users/Kayrat/Desktop'
+
+
+def main():
+    app = QApplication(sys.argv)
+    _yt_downloader = YouTubeK()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
